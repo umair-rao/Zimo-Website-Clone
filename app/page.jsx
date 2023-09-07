9d0fab70d0047ce1ec36288a6f0342bc7d84f8de
Umair Rao <umairmuqeem@gmail.com>
2023-09-07 12:53:07 +0500

    Page 2 Error Removed

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import Page1 from "@components/Page1";
-import Page2 from "@components/page2";
+import Page2 from "@components/Page2";
 import Page3 from "@components/Page3";
 import Page4 from "@components/Page4";
 import Page5 from "@components/Page5";
